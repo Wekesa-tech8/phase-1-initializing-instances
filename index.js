791041a50c7cdf6aaaf8bb25439615c6d053006f
David Wekesa <wekesadavid300@gmail.com>
2024-07-16 17:10:01 +0300

    Instances

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+class Dinner {
+  #dessert; 
+
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.#dessert = dessert; 
+  }
+  getDessert() {
+    return this.#dessert;
+  }
+}
+let breakfast1 = new Breakfast("Pancakes", "Coffee");
+let breakfast2 = new Breakfast("Omelette", "Orange Juice");
+
+console.log(breakfast1); 
+console.log(breakfast2); 
+
+let lunch1 = new Lunch("Caesar Salad", "Tomato Soup", "Lemonade");
+let lunch2 = new Lunch("Greek Salad", "Chicken Noodle Soup", "Iced Tea");
+
+console.log(lunch1); 
+console.log(lunch2); 
+
+let dinner1 = new Dinner("House Salad", "Minestrone", "Steak", "Cheesecake");
+let dinner2 = new Dinner("Garden Salad", "Clam Chowder", "Salmon", "Tiramisu");
+
+console.log(dinner1); 
+console.log(dinner2); 
+
+console.log(dinner1.getDessert()); 
+console.log(dinner2.getDessert()); 
